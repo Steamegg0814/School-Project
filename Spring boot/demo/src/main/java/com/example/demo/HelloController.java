cef5b53ff23263a10f5e7a72085b50b78556973e
Steamegg0814 <47239676+Steamegg0814@users.noreply.github.com>
2024-04-24 14:38:03 +0800

    upload  project

---
+++ Spring boot/demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping(value = "/")
+	public String spring() {
+		return "Spring Boot!!";
+	}
+	
+	@RequestMapping(value = "/hello")
+	public String hello() {
+		return "Hello World!!";
+	}
+
+}
